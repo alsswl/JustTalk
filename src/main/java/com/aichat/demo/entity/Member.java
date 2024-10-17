0aa60d48540c2213ffd034413e13d28d481faf25
Minji Kim <101392857+alsswl@users.noreply.github.com>
2024-10-17 22:31:41 +0900

    feat : member getter,setter 추가

--- src/main/java/com/aichat/demo/entity/Member.java
+++ src/main/java/com/aichat/demo/entity/Member.java
@@ -4,12 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 import jakarta.persistence.GeneratedValue;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Member {
 
   @Id
@@ -19,6 +24,8 @@ public class Member {
   private String name;
   private String email;
   private String password;
+  @Lob
+  private String nowContent = "";
 
   @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   private List<Report> reports;
