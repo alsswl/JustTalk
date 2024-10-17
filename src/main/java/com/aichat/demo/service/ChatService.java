e63f32b120b3bbfe88a80054df9964049aeb64b9
Minji Kim <101392857+alsswl@users.noreply.github.com>
2024-10-17 22:32:53 +0900

    feat : 모델 별 다른 ai 메서드 호출

--- src/main/java/com/aichat/demo/service/ChatService.java
+++ src/main/java/com/aichat/demo/service/ChatService.java
@@ -2,7 +2,9 @@ package com.aichat.demo.service;
 
 import com.aichat.demo.dto.request.ChatRequest;
 import com.aichat.demo.dto.response.ChatResponse;
+import com.aichat.demo.entity.Member;
 import com.aichat.demo.infrastructure.OpenAiService;
+import com.aichat.demo.repository.MemberRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -14,12 +16,119 @@ public class ChatService {
 
   private final SimpMessagingTemplate messagingTemplate;
   private final OpenAiService openAiService;
+  private final JwtService jwtService;
+  private final MemberRepository memberRepository;
 
-  public void processMessage(ChatRequest.ChatMessageDTO requestDTO) throws JsonProcessingException {
+  public void processMessage1(ChatRequest.ChatMessageDTO requestDTO) throws JsonProcessingException {
     String userMessage = requestDTO.getContent();
-    String aiResponse = openAiService.getResponseFromAi(userMessage);
+    String token = requestDTO.getToken();
+    String email = "";
+    if (token != null && !token.isEmpty()) {
+      email = jwtService.getMemberEmail(token);
+      System.out.println(email);
+    } else {
+      throw new IllegalArgumentException("Token is missing");
+    }
+
+    Member member = memberRepository.findByEmail(email).get();
+    String memberContent = member.getNowContent();
+    String aiResponse = openAiService.getResponseFromWogigi(userMessage,memberContent);
+    memberContent = memberContent + userMessage + aiResponse;
+
+    if (memberContent.length() > 5000) {
+      //리포트
+      memberContent = memberContent.substring(memberContent.length() - 5000);
+    }
+
+    member.setNowContent(memberContent);
+    memberRepository.save(member);
 
     ChatResponse.ChatMessageDTO aiMessage = new ChatResponse.ChatMessageDTO(aiResponse);
     messagingTemplate.convertAndSend("/topic/messages", aiMessage);
   }
+
+  public void processMessage2(ChatRequest.ChatMessageDTO requestDTO) throws JsonProcessingException {
+    String userMessage = requestDTO.getContent();
+    String token = requestDTO.getToken();
+    String email = "";
+    if (token != null && !token.isEmpty()) {
+      email = jwtService.getMemberEmail(token);
+      System.out.println(email);
+    } else {
+      throw new IllegalArgumentException("Token is missing");
+    }
+
+    Member member = memberRepository.findByEmail(email).get();
+    String memberContent = member.getNowContent();
+    String aiResponse = openAiService.getResponseFromAung(userMessage,memberContent);
+    memberContent = memberContent + userMessage + aiResponse;
+
+    if (memberContent.length() > 5000) {
+      //리포트
+      memberContent = memberContent.substring(memberContent.length() - 5000);
+    }
+
+    member.setNowContent(memberContent);
+    memberRepository.save(member);
+
+    ChatResponse.ChatMessageDTO aiMessage = new ChatResponse.ChatMessageDTO(aiResponse);
+    messagingTemplate.convertAndSend("/topic/messages", aiMessage);
+  }
+
+  public void processMessage3(ChatRequest.ChatMessageDTO requestDTO) throws JsonProcessingException {
+    String userMessage = requestDTO.getContent();
+    String token = requestDTO.getToken();
+    String email = "";
+    if (token != null && !token.isEmpty()) {
+      email = jwtService.getMemberEmail(token);
+      System.out.println(email);
+    } else {
+      throw new IllegalArgumentException("Token is missing");
+    }
+
+    Member member = memberRepository.findByEmail(email).get();
+    String memberContent = member.getNowContent();
+    String aiResponse = openAiService.getResponseFromBuBu(userMessage,memberContent);
+    memberContent = memberContent + userMessage + aiResponse;
+
+    if (memberContent.length() > 5000) {
+      //리포트
+      memberContent = memberContent.substring(memberContent.length() - 5000);
+    }
+
+    member.setNowContent(memberContent);
+    memberRepository.save(member);
+
+    ChatResponse.ChatMessageDTO aiMessage = new ChatResponse.ChatMessageDTO(aiResponse);
+    messagingTemplate.convertAndSend("/topic/messages", aiMessage);
+  }
+
+  public void processMessage4(ChatRequest.ChatMessageDTO requestDTO) throws JsonProcessingException {
+    String userMessage = requestDTO.getContent();
+    String token = requestDTO.getToken();
+    String email = "";
+    if (token != null && !token.isEmpty()) {
+      email = jwtService.getMemberEmail(token);
+      System.out.println(email);
+    } else {
+      // 토큰이 없을 경우 처리
+      throw new IllegalArgumentException("Token is missing");
+    }
+
+    Member member = memberRepository.findByEmail(email).get();
+    String memberContent = member.getNowContent();
+    String aiResponse = openAiService.getResponseFromCoCo(userMessage,memberContent);
+    memberContent = memberContent + userMessage + aiResponse;
+
+    if (memberContent.length() > 5000) {
+      memberContent = memberContent.substring(memberContent.length() - 5000);
+    }
+
+    member.setNowContent(memberContent);
+    memberRepository.save(member);
+
+    ChatResponse.ChatMessageDTO aiMessage = new ChatResponse.ChatMessageDTO(aiResponse);
+    messagingTemplate.convertAndSend("/topic/messages", aiMessage);
+  }
+
 }
