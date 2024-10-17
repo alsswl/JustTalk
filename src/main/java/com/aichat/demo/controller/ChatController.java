f6ff59e4b0922e8c1aa17296f82ee068559d796f
Minji Kim <101392857+alsswl@users.noreply.github.com>
2024-10-17 22:31:06 +0900

    feat : 캐릭터 별 모델 분리

--- src/main/java/com/aichat/demo/controller/ChatController.java
+++ src/main/java/com/aichat/demo/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.aichat.demo.controller;
 
 import com.aichat.demo.dto.request.ChatRequest;
 import com.aichat.demo.service.ChatService;
+import com.aichat.demo.service.JwtService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Controller;
 
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,13 +19,36 @@ public class ChatController {
 
   private final ChatService chatService;
 
-  @MessageMapping("/chat.sendMessage")
-  public void sendMessage(@Payload ChatRequest.ChatMessageDTO chatMessage)
+  @PostMapping("/chat/{Id}")
+  public void startChat(@PathVariable Integer Id){
+
+  }
+
+  @MessageMapping("/chat.sendMessage1")
+  public void sendMessage1(@Payload ChatRequest.ChatMessageDTO chatMessage)
       throws JsonProcessingException {
-    chatService.processMessage(chatMessage);
+    chatService.processMessage1(chatMessage);
+  }
+  @MessageMapping("/chat.sendMessage2")
+  public void sendMessage2(@Payload ChatRequest.ChatMessageDTO chatMessage)
+      throws JsonProcessingException {
+    chatService.processMessage2(chatMessage);
+  }
+  @MessageMapping("/chat.sendMessage3")
+  public void sendMessage3(@Payload ChatRequest.ChatMessageDTO chatMessage)
+      throws JsonProcessingException {
+    chatService.processMessage3(chatMessage);
+  }
+  @MessageMapping("/chat.sendMessage4")
+  public void sendMessage4(@Payload ChatRequest.ChatMessageDTO chatMessage)
+      throws JsonProcessingException {
+    chatService.processMessage4(chatMessage);
   }
   @MessageMapping("/chat.addUser")
   public void addUser(@Payload ChatRequest.ChatMessageDTO chatMessage, SimpMessageHeaderAccessor headerAccessor) {
     headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
   }
+
+
+
 }
